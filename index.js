43bc3d7250b2e3e24096fbf91135c586cca98bca
Aris Vayas <aris.vayas@gmail.com>
2022-04-04 19:43:05 -0400

    commit tree 24b71aec6d108631df755c511d6a82b7e2286f7e

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
 
-// Write your code here
+
+
+let num1=1
+let num2 =62
+
+
+
+
+let multiply = num1 *num2 
+
+ let random = Math.floor(Math.random()*11);
+ 
+
+
+
+let num3 = 40
+let num4 =9
+let mod = num3%num4
+
+let max = Math.max(2,20,9,5,6)
